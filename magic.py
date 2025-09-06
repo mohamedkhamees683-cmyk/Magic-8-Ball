2c0d608460422c435011fe78f9195b4142c9ead5
mohamedkhamees683-cmyk <mohamedkhamees683@gmail.com>
2025-09-06 18:59:00 +0300

    the returns

---
+++ magic.py
@@ -0,0 +1,10 @@
+import random
+responses=[
+"yes,definitely!",
+"No,not now","Ask Again Later",
+"It is certain",
+"Very doubt ful",
+"Out look is good",
+"Better not tell you now",
+"concentrate and ask again"
+]
